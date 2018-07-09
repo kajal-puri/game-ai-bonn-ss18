9e19d218f34801ce2171e02c18f0d910d5e6e14e
Omid Najaee <o.najaee@gmail.com>
2018-07-09 09:31:26 +0200

    add bayesean biases

--- project-3/som.py
+++ project-3/som.py
@@ -2,10 +2,14 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import euclidean
+
+
 import sys
 
 
-def init_circular_weights(data, length=10):
+def init_circular_weights(data, length = 10):
   length += 1
 
   weights = np.zeros((length, data.shape[1]))
@@ -30,8 +34,7 @@ def init_circular_weights(data, length=10):
 
   return weights
 
-
-def SOM(data, weights, shuffled_indices, learning_rate=1):
+def SOM(data, weights, shuffled_indices, learning_rate=0.25):
   graphs = []
   graph = np.copy(weights)
   tmax = len(shuffled_indices)
@@ -50,27 +53,24 @@ def SOM(data, weights, shuffled_indices, learning_rate=1):
     c = np.argmin(np.linalg.norm(graph - data[index, :], axis=1))
 
     eta = learning_rate * (1 - 1.0 * t / tmax)
-
     sigma = np.exp(- 1.0 * t / tmax)
-
     updateRule = eta * np.exp(-1.0 * distances[c, :] / (2.0 * sigma))
 
     graph = graph + np.multiply(updateRule, (data[index, :] - graph).T).T
 
     graphs.append(np.copy(graph))
 
-  return graph, graphs  
+  return graph, graphs
 
 
 if __name__ == "__main__":
   dataFile = sys.argv[1] if len(sys.argv) >= 2 else 'q3dm1-path1.csv'
-  graphLength = int(sys.argv[2]) if len(sys.argv) >= 3 else 10
+  n_vertices = int(sys.argv[2]) if len(sys.argv) >= 3 else 36
   learning_rate = float(sys.argv[3]) if len(sys.argv) >= 4 else 1
 
   data = np.genfromtxt(dataFile, delimiter=',')
-
-  weights = init_circular_weights(data, graphLength)
-
+  weights = init_circular_weights(data, n_vertices)
+  
   fig = plt.figure()
   ax = fig.add_subplot(111, projection="3d")
 
@@ -88,6 +88,12 @@ if __name__ == "__main__":
     markerfacecolor='r', markeredgecolor='k', color='b')
 
   selectedPoint, = ax.plot([], [], [], marker = 'o', markerfacecolor='k', markeredgecolor='k')
+  # guessPoint, = ax.plot([], [], [], marker = 'o', markerfacecolor='k', markeredgecolor='k')
+  def updateGuessPoint(index):
+    point = points[index]
+    guessPoint.set_data(point[0], point[1])
+    guessPoint.set_3d_properties(point[2])
+    return guessPoint;
 
   def updateSelectedPoint(index):
     selectedPoint.set_data(data[index, 0], data[index, 1])
@@ -103,11 +109,64 @@ if __name__ == "__main__":
 
   indices = np.arange(data.shape[0])
   np.random.shuffle(indices)
-
+  # data = np.random.permutation(data)
   graph, updateGraphs = SOM(data, weights, indices, learning_rate)
 
-  ani_1 = FuncAnimation(fig, updateSOM, frames=len(updateGraphs))
+  activities = np.diff(data, axis=0)
+  kmeans = KMeans(n_clusters=n_vertices).fit(activities)
+  activities = kmeans.labels_
 
-  ani_2 = FuncAnimation(fig, updateSelectedPoint, frames=indices)
+  states = []
+  for i in range(data.shape[0]):
+    minValue = np.inf
+    minIndex = -1
+    for j in range(graph.shape[0]):
+      dist = euclidean(data[i], graph[j])
+      if dist < minValue:
+        minValue = dist
+        minIndex = j
+    states.append(minIndex)
+  states = np.array(states)
+
+  p = np.zeros((n_vertices, n_vertices))
+
+  for i in range(len(data) - 1):
+    activity = activities[i]
+    state = states[i]
+    p[state][activity] += 1
+
+  # p *= 1.0 / data.shape[0]
+
+  # normalize
+  for i in range(len(p)):
+    p[i] = p[i] / np.sum(p[i])
+
+  index = np.random.choice(np.arange(data.shape[0]), 1)[0]
+  x = data[index]
+  points = []
+  for i in range(index, len(data)):
+    state = states[i]
+    a = kmeans.cluster_centers_[np.argmax(p[state,:] / np.sum(p, axis=1)[state] )]
+    x = x + a
+    points.append(x)
+
+  points = np.array(points)
+
+  trPoint, = ax.plot(
+    [data[index][0]], [data[index][1]], [data[index][2]], marker = 'o',
+    markerfacecolor='b', markeredgecolor='b', color='b', 
+  )
+  
+  ax.scatter(points[:, 0], points[:, 1], points[:, 2], c='g', edgecolors='w')
+
+  ani_1 = FuncAnimation(fig, updateSOM, frames=len(updateGraphs), interval=300)
+  ani_2 = FuncAnimation(fig, updateSelectedPoint, frames=indices, interval=300)
+
+  def updateTr(i):
+    trPoint.set_data(points[i][0],points[i][1])
+    trPoint.set_3d_properties(points[i][2])
+    return trPoint
+
+  ani_3 = FuncAnimation(fig, updateTr, frames=len(data) - index, interval=200)
 
   plt.show()
